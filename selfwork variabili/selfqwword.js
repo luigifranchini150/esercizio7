0472494a95a6a834094b69cddaebeb2cf19e9373
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-03 15:26:20 +0200

    Add files via upload

---
+++ selfwork variabili/selfqwword.js
@@ -0,0 +1,12 @@
+let num1 = 45
+let num2 = 5
+// il valore della variabile num1 è 45
+// il valore della variabile num2 è 5
+console.log("il valore della variabile num1 è :"+ num1);
+console.log("il valore della variabile num2 è :"+ num2);
+let stringa = "Ciao a tutti"
+stringa = "benvenuti"
+console.log("il valore della variabile stringa è :"+ stringa);
+const pGreco = 3.14;
+pGreco = 100;
+console.log("il valore di pGreco è :"+ pGreco);
