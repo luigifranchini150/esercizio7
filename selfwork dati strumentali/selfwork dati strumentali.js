bd722ba611743d4012e068d8190e1cbeba6f9b42
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-13 16:59:55 +0200

    Add files via upload

---
+++ selfwork dati strumentali/selfwork dati strumentali.js
@@ -0,0 +1,38 @@
+let dato1 = [
+[`un`,`per`,`incatenarli.`],
+[`Anello`,`trovarli,`],
+[`ghermirli`,`e`],
+[`gondor`,`mark`],
+];
+let dato2 = [
+[[`trovarli,`]],
+[`tu`,`sciocchi`],
+[`tu`,`schiocchi`,[`padron`,`sauron`]],
+[`nel`,[`fuggite`,`gandalf`]],
+[[`domarli,`,`passare`],`buio`]
+];
+console.log(
+    dato1[0][0],
+    dato1[1][0],
+    dato1[0][1],
+    dato2[4][0][0],
+    dato1[0][0],
+    dato1[1][0],
+    dato1[0][1],
+    dato1[1][1],
+    dato1[0][0],
+    dato1[1][0],
+    dato1[0][1],
+    dato1[2][0],
+    dato1[2][1],
+    dato2[3][0],
+    dato2[4][1],
+    dato1[0][2],
+);
+
+
+
+
+
+
+
